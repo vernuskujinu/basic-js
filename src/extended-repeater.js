e695c1b50bc4515ffdb0743573ad0b90136a3c30
vernuskujinu <78141473+vernuskujinu@users.noreply.github.com>
2021-09-08 10:36:26 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,29 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let { 
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+
+  let resStr = '';
+
+  let additionStr = '';
+
+  if (addition !== '') {
+    additionStr = repeater(addition, {repeatTimes: additionRepeatTimes, separator: additionSeparator});
+  }
+
+  if (repeatTimes > 0) resStr = str + additionStr;
+  repeatTimes--;
+  
+  for (let i = 0; i < repeatTimes;) {
+    resStr += (separator + str + additionStr);
+    repeatTimes--;
+  }
+  return resStr;
 }
