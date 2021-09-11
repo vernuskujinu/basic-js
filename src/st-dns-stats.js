a100ef70303054f8643706356623841bca216f57
vernuskujinu <78141473+vernuskujinu@users.noreply.github.com>
2021-09-11 16:14:01 +0300

    Update st-dns-stats.js

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -22,7 +22,34 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats( domains ) {
+  const arr = [];
+  let maxLevel = 0;
+  domains.forEach((el) => {
+    const tmp = el.split('.');
+    if (tmp.length > maxLevel) maxLevel = tmp.length;
+    arr.push(tmp.reverse());
+  });
+  let level = 1;
+  const res = {};
+  while (level <= maxLevel) {
+    for (let k = 0; k < arr.length; k++) {
+      const el = arr[k];
+      let domainName = '';
+      if (el.length >= level) {
+        for (let i = 0; i < level; i++) {
+          domainName += `.${el[i]}`;
+        }
+      }
+      if (domainName !== '') {
+        if (res[domainName]) {
+          res[domainName]++;
+        } else {
+          res[domainName] = 1;
+        }
+      }
+    }
+    level++;
+  }
+  return res;
 }
