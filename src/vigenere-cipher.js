8d7a0df57282bdbe189c6dcc177d0580311d0173
vernuskujinu <78141473+vernuskujinu@users.noreply.github.com>
2021-09-11 16:19:05 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,50 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  isDirect = true;
+  AL = 26;
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key)
+      throw new Error(`Incorrect arguments!`);
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let keyString = "";
+    for (let i = 0; i < message.length; i++)
+      keyString += key[i % key.length];
+    let res = "";
+    for (let i = 0, count = 0; i < message.length; i++) {
+      const elCode = message[i].charCodeAt(0);
+      if (elCode < 65 || elCode > 90) {
+        res += message[i];
+        continue;
+      }
+      const keyCode = keyString[count].charCodeAt(0);
+      count++;
+      res += String.fromCharCode(((keyCode + elCode) % this.AL) + 65)
+    }
+    return this.isDirect ? res : res.split("").reverse().join("");
+  }
+  decrypt(message, key) {
+    if (!message || !key)
+      throw new Error(`Incorrect arguments!`);
+    key = key.toUpperCase();
+    let keyString = "";
+    for (let i = 0; i < message.length; i++)
+      keyString += key[i % key.length];
+    let res = "";
+    for (let i = 0, count = 0; i < message.length; i++) {
+      const elCode = message[i].charCodeAt(0);
+      if (elCode < 65 || elCode > 90) {
+        res += message[i];
+        continue;
+      }
+      const keyCode = keyString[count].charCodeAt(0);
+      count++;
+      res += String.fromCharCode(((elCode + this.AL - keyCode) % this.AL) + 65)
+    }
+    return this.isDirect ? res : res.split("").reverse().join("");
   }
 }
